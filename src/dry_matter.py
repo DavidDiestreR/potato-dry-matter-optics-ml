b9b8be57df1d20c6884cbe9c84153fb3a912044c
DavidDiestreR <david.diestre.r@gmail.com>
2025-12-05 11:58:37 +0100

    estructura de src

---
+++ src/dry_matter.py
@@ -0,0 +1,32 @@
+"""
+Mòdul: dry_matter
+Funcions relacionades amb la predicció i classificació de la matèria seca.
+"""
+
+
+def predict_dm(features, scaler, model):
+    """
+    Prediu la matèria seca (DM) a partir d'un vector de característiques.
+
+    Paràmetres
+    ----------
+    features : Any
+        dataframe amb les característiques d'entrada.
+    scaler : Any
+        Objecte per escalar les features.
+    model : Any
+        Model de machine learning entrenat per fer la predicció.
+    """
+    pass
+
+
+def dry_matter_quality_classification(dm_value):
+    """
+    Classifica la qualitat de la patata en funció de la matèria seca.
+
+    Paràmetres
+    ----------
+    dm_value : float
+        Valor de matèria seca (% o la unitat que decidiu).
+    """
+    pass
---
+++ src/raw_image_treatment.py
@@ -0,0 +1,28 @@
+"""
+Mòdul: raw_image_treatment
+Funcions relacionades amb el tractament inicial d'imatges de patates.
+"""
+
+
+def potato_defect_classification(image):
+    """
+    Classifica els defectes de la patata a partir d'una imatge.
+
+    Paràmetres
+    ----------
+    image : Any
+        Objecte imatge ja carregat (per exemple, numpy array, PIL.Image, etc.).
+    """
+    pass
+
+
+def potato_min_square(image):
+    """
+    Calcula el quadrat mínim (bounding box) que conté la patata a la imatge.
+
+    Paràmetres
+    ----------
+    image : Any
+        Objecte imatge ja carregat.
+    """
+    pass
